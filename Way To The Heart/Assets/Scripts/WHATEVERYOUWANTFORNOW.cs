1515f8d7b3a80f3efd52a21d9b32643db3b5e2ea
spicyseabanana <catalystcomi@gmail.com>
2025-10-15 18:21:07 -0700

    LETSFUCKINGGOOOO

---
+++ Way To The Heart/Assets/Scripts/WHATEVERYOUWANTFORNOW.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public static class WHATEVERYOUWANTFORNOW
+{
+    // actions left
+    public static int actions = 12;
+    // day of the week
+    public static int week = 1;
+    // day of the season
+    public static int date = 30;
+    // season of the year
+    public static int season = 4;
+    // time of day
+    // when actions>10 = morning
+    // amount of gold
+    public static int gold = 0;
+   
+}
---
+++ Way To The Heart/Assets/Scripts/testie.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+
+public class testie : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text.text = WHATEVERYOUWANTFORNOW.gold.ToString();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
