a87db8894f191306ff224a0bdb2f9e22dfff2bda
spicyseabanana <catalystcomi@gmail.com>
2025-10-21 02:39:09 -0700

    AAAAAA

--- Way To The Heart/Assets/Scripts/WHATEVERYOUWANTFORNOW.cs
+++ Way To The Heart/Assets/Scripts/WHATEVERYOUWANTFORNOW.cs
@@ -3,16 +3,54 @@ using UnityEngine;
 public static class WHATEVERYOUWANTFORNOW
 {
     // actions left
+    // things needed: create an "action"
+    // when an "action" is performed, the current # of actions minus 1
+    // when action value=0, trigger the end of the day cutscene, increase the "week" and "date" values by 1, and reset action count
     public static int actions = 12;
+    if (actions == 0)
+    {
+        week = week + 1;
+        date = date + 1;
+        actions = 12;
+    }
     // day of the week
+    // when action value is 0, increase the week value by 1
+    // when action value is greater than 7, reset to 1.
+    // assign weekdays to each number.
     public static int week = 1;
     // day of the season
-    public static int date = 30;
+    // when action value=0, increase the date value by 1
+    // when week is greater than 30, trigger the end of season cutscene, increase the season value by 1, and reset date to 1
+    public static int date = 1;
     // season of the year
-    public static int season = 4;
+    // assign each number to a different season (Spring, Summer, Fall, Winter)
+    // when date is greater than 30, increase the season value by 1
+    public static int season = 1;
     // time of day
-    // when actions>10 = morning
+    // assign each number to a different time of day (Morning, Noon, Afternoon, Evening)
+    // when action value is 10, 11, or 12 = 1
+    // when action value is 7, 8, or 9 = 2
+    // when action value is 4, 5, 6 = 3
+    // when action value is 1, 2, 3 = 4
+    public static int time = 1;
     // amount of gold
     public static int gold = 0;
-   
+    
+    static void ActionCheck()
+    {
+        if (actions == 0)
+        {
+            week = week + 1;
+            date = date + 1;
+            actions = 12;
+        }
+    }
+    static void WkCheck()
+    {
+        if (week > 7)
+        {
+            week = 1;
+        }
+    }
+    
 }
--- Way To The Heart/Assets/Scripts/testie.cs
+++ Way To The Heart/Assets/Scripts/testie.cs
@@ -4,15 +4,49 @@ using TMPro;
 public class testie : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public TextMeshProUGUI text2;
+    public TextMeshProUGUI week;
+    public TextMeshProUGUI date;
+    public TextMeshProUGUI season;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        text.text = WHATEVERYOUWANTFORNOW.gold.ToString();   
+        
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+        text.text = WHATEVERYOUWANTFORNOW.gold.ToString();
+        text2.text = WHATEVERYOUWANTFORNOW.actions.ToString();
+        date.text = WHATEVERYOUWANTFORNOW.date.ToString();
+
+        if (WHATEVERYOUWANTFORNOW.week == 1) 
+        {
+            week.text = "Monday";
+        }
+        if (WHATEVERYOUWANTFORNOW.week == 2) 
+        {
+            week.text = "Tuesday";
+        }
+        if (WHATEVERYOUWANTFORNOW.week == 3) 
+        {
+            week.text = "Wednesday";
+        }
+        if (WHATEVERYOUWANTFORNOW.week == 4)
+        {
+            week.text = "Thursday";
+        }
+        if (WHATEVERYOUWANTFORNOW.week == 5) 
+        {
+            week.text = "Friday";
+        }
+
+        if (WHATEVERYOUWANTFORNOW.season == 1) 
+        {
+            season.text = "SPRING";
+        }
         
     }
 }
