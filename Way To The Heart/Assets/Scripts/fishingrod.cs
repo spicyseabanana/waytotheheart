ed32654fa872bc6e23c9758a50cd5b1cfac8e0f5
spicyseabanana <catalystcomi@gmail.com>
2025-10-29 20:02:12 -0700

    weh

--- Way To The Heart/Assets/Scripts/fishingrod.cs
+++ Way To The Heart/Assets/Scripts/fishingrod.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class fishingrod : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Image fishrod;
-    void Start()
-    {
-        
-    }
+[RequireComponent(typeof(BoxCollider))]
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.localPosition = Input.mousePosition;
-    }
+public class GizmosController : MonoBehaviour 
+{
+
+private Vector3 screenPoint;
+private Vector3 offset;
+
+void OnMouseDown()
+{
+    screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+}
+
+void OnMouseDrag()
+{
+    Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+transform.position = curPosition;
+
+}
+
 }
