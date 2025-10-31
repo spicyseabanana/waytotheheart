37498ad501f98551eafde6b2c640d51088323f21
spicyseabanana <catalystcomi@gmail.com>
2025-10-31 00:28:21 -0700

    screaming

--- Way To The Heart/Assets/Scripts/fishingrod.cs
+++ Way To The Heart/Assets/Scripts/fishingrod.cs
@@ -1,29 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(BoxCollider))]
-
-public class GizmosController : MonoBehaviour 
+public class fishingrod : MonoBehaviour, IPointerDownHandler
 {
-
-private Vector3 screenPoint;
-private Vector3 offset;
-
-void OnMouseDown()
-{
-    screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-
-    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
 }
 
-void OnMouseDrag()
-{
-    Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-transform.position = curPosition;
-
-}
-
-}
